8bd41de67ed9341e81cbb24b0477373d6fa97d23
Rodrigo Casio <rod.casio@outlook.com>
2025-03-27 11:23:37 -0600

    Create fizzBuzz.c

---
+++ fizzBuzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  //by rodcasio
+  int numbers = 100;
+
+  for(int i = 1; i <= 100; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+      printf("FizzBuzz\n");
+    }
+    else if(i % 3 == 0){
+      printf("Fizz\n");
+    }
+    else if(i % 5 == 0){
+      printf("Buzz\n");
+    }
+    else{
+      printf("%d\n", i);
+    }
+  }
+
+}
