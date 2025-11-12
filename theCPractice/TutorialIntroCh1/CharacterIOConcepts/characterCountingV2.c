7c68367820faa9cc1f68cb046198c451fe0cc1ac
rodrigcasio <rod.casio@outlook.com>
2025-11-11 19:17:57 -0600

    Add new example for `CountingCharacters`
    this example uses `for (nc = 0; getchar() != EOF; ++nc) ;` to count number of character within the buffer

---
+++ theCPractice/TutorialIntroCh1/CharacterIOConcepts/characterCountingV2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+// count characters in input v2 (for ())
+int main() {
+  
+  double nc;
+
+  for (nc = 0; getchar() != EOF; ++nc)
+    ; // ; <-- null statement
+
+  printf("%.0f", nc);
+}
+
+/* TEST (output "1")
+ *
+ * 1. run program
+ * 2. type any character (e) [it sits at the buffer]
+ * 3. Press Ctrl + D once (flushes the buffer program, nc becomes = 1)
+ * 4. The program immediately asks for input again (cursor likely stays on the same line)
+ * 5. Press Ctrl + D a second time (sends the EOF signal, terminates the loop)
+ * 6. Program outputs 1
+
+/*  TEST (getting output "2")
+ *  cc characterCounting.c 
+ *  ./a.out 
+ *
+* e
+* (Press Enter)
+* (Ctrl + D [signal EOF]) 
+* 2 <---- output two characters (1: e, 2: Enter)
+*
+* Enter key: sends the buffer content plus a new linn character (\n)
+* Ctrl + D: sends the buffer content without a newline
+* Ctrl + D on an empty buffer: sends the EOF signal (zero bytes read)
+*
+*
+*/
