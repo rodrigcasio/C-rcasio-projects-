819ebde1b3b2266d02fd6d15d04cc7c3b9a7fd5c
rodrigcasio <rod.casio@outlook.com>
2025-11-19 17:05:34 -0600

    Add new example for 1-15 exercise

---
+++ theCPractice/TutorialIntroCh1/Functions/functionEx1-15.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* Rewrite the temperature conversion program of Section1.2 to use a function conversion */
+
+float toCelsius(int n);
+float toFahr(int n);
+
+int main(){
+  
+  
+
+  return 0;
+}
+
+float toCelsius(int t_fahr) {
+  temp = (5.0/9.0) * (t_fahr - 32.0);
+  return temp;
+}
+
